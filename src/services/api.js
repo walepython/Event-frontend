58d56df47e8a4178ce2d04ce37fe46ecff0f9922
@waminisce <onidareadewaleoluwafemi69@gmail.com>
2026-02-19 11:28:57 +0100

    Fix API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://advance-django-event.onrender.com",
 });
 
 // Helper to get access token
